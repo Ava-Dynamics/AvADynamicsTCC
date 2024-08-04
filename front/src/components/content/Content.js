4ce532c4c89dad0c7177df5db90d6dee1863bb4d
gyovanna <gyovannacarvalho@gmail.com>
2024-08-04 18:21:23 -0300

    feat: adding new images to hero page

--- front/src/components/content/Content.js
+++ front/src/components/content/Content.js
@@ -1,27 +1,29 @@
 function Content({ title, description, subDescription, position, image }) {
   return (
-    <div id="features" className="h-full md:h-[calc(100vh-10rem)]">
+    <div id='features' className='h-full md:h-[calc(100vh-10rem)]'>
       <div
-        className={`flex justify-between
-        ${position === "left" ? "flex-row-reverse" : "flex-row"}
+        className={`flex flex-col md:flex-row justify-between
+        ${position === 'left' ? 'md:flex-row-reverse' : 'md:flex-row'}
       `}
       >
-        <div className="md:max-w-md lg:max-w-lg">
+        <div className='md:max-w-md lg:max-w-lg p-4'>
           <div>
-            <span className="text-finscorePurple md:text-5xl lg:text-6xl">
+            <span className='text-finscorePurple text-3xl md:text-5xl lg:text-6xl'>
               {title}
             </span>
           </div>
-          <div className="mt-5 flex flex-col gap-5">
-            <span className="md:text-md lg:text-xl">{description}</span>
-            <span className="md:text-md lg:text-xl">{subDescription}</span>
+          <div className='mt-5 flex flex-col gap-5'>
+            <span className='text-sm md:text-md lg:text-xl'>{description}</span>
+            <span className='text-sm md:text-md lg:text-xl'>
+              {subDescription}
+            </span>
           </div>
         </div>
-        <div>
+        <div className='mt-5 md:mt-0 md:max-w-[50%] flex justify-center items-center'>
           <img
-            className="rounded-3xl"
-            src="https://via.placeholder.com/300"
-            alt="placeholder"
+            className='rounded-3xl w-full h-auto'
+            src={image}
+            alt='placeholder'
           />
         </div>
       </div>
--- front/src/components/hero/Hero.js
+++ front/src/components/hero/Hero.js
@@ -1,23 +1,27 @@
-import Footer from "../Footer/Footer";
-import About from "../about-us/About";
-import Contact from "../contact/Contact";
-import Content from "../content/Content";
-import Navbar from "../navbar/Navbar";
+import Footer from '../Footer/Footer'
+import About from '../about-us/About'
+import Contact from '../contact/Contact'
+import Content from '../content/Content'
+import Navbar from '../navbar/Navbar'
+import HomepageImage1 from '../../assets/homepage-1.jpg'
+import HomepageImage2 from '../../assets/homepage-2.jpg'
+import HomepageImage3 from '../../assets/homepage-3.jpg'
+import HomepageImage4 from '../../assets/homepage-4.jpg'
 
 function Hero() {
   return (
-    <div className="antialiased min-h-screen h-full px-7 md:px-10 xl:px-28 2xl:px-72">
+    <div className='antialiased min-h-screen h-full px-7 md:px-10 xl:px-28 2xl:px-72'>
       <Navbar />
-      <div className="h-full md:h-[calc(100vh-16rem)] flex flex-col justify-center">
-        <div className="flex items-center justify-between">
-          <div className="flex flex-col text-5xl">
+      <div className='h-full md:h-[calc(100vh-16rem)] flex flex-col justify-center'>
+        <div className='flex items-center justify-between'>
+          <div className='flex flex-col text-5xl'>
             <span>você</span>
-            <span className="text-finscorePurple blink">financeiramente</span>
+            <span className='text-finscorePurple blink'>financeiramente</span>
             <span>+ saudável</span>
             <div>
               <button
-                className="mt-10 bg-gradient-to-r from-purple-400 via-purple-800 to-purple-900 hover:from-purple-400 hover:via-purple-400
- hover:to-purple-400 px-10 py-3 rounded-3xl text-xl transition-all duration-900 hover:fade-in"
+                className='mt-10 bg-gradient-to-r from-purple-400 via-purple-800 to-purple-900 hover:from-purple-400 hover:via-purple-400
+ hover:to-purple-400 px-10 py-3 rounded-3xl text-xl transition-all duration-900 hover:fade-in'
               >
                 Saiba mais
               </button>
@@ -25,36 +29,37 @@ function Hero() {
           </div>
           <div>
             <img
-              className="rounded-3xl"
-              src="https://via.placeholder.com/300"
-              alt="placeholder"
+              className='rounded-3xl'
+              src={HomepageImage1}
+              alt='placeholder'
             />
           </div>
         </div>
       </div>
       <About />
       <Content
-        title="Acompanhe a evolução do seu Score de Crédito"
-        description="Use a Finscore para acompanhar o progresso do seu score de crédito."
-        subDescription="Nossa missão é prover de gatilhos de cuidados com o seu financeiro para que o seu score de crédito aumente, afinal: queremos o nosso nome “limpo na praça”."
+        title='Acompanhe a evolução do seu Score de Crédito'
+        description='Use a Finscore para acompanhar o progresso do seu score de crédito.'
+        subDescription='Nossa missão é prover de gatilhos de cuidados com o seu financeiro para que o seu score de crédito aumente, afinal: queremos o nosso nome “limpo na praça”.'
+        image={HomepageImage2}
       />
       <Content
-        position="left"
-        title="Monitore seus gastos e reservas"
-        description="O sistema inteligente da Finscore possibilita que você monitore seus gastos fixos e variáveis afim de entender quais são seu ofensores e como você pode evoluir financeiramente. 
-  "
-        subDescription="Nossos gráficos e inteligencia artificial permite que seus dados de pagamento cadastrados na plataforma sejam demonstrado de maneira simples e didática, informando e exemplicando boas práticas de melhora de capital."
+        position='left'
+        title='Monitore seus gastos e reservas'
+        description='O sistema inteligente da Finscore possibilita que você monitore seus gastos fixos e variáveis afim de entender quais são seu ofensores e como você pode evoluir financeiramente.'
+        subDescription='Nossos gráficos e inteligencia artificial permite que seus dados de pagamento cadastrados na plataforma sejam demonstrado de maneira simples e didática, informando e exemplicando boas práticas de melhora de capital.'
+        image={HomepageImage3}
       />
       <Content
-        title="Construa sua carteira de investimentos"
-        description="A Finscore traz cursos de como, onde e quando investir seu dinheiro para que você saia das dívidas, tenha uma reseva de emergencia e investa seu dinheiro para planos futuros. 
-  "
-        subDescription="Além disso você pode usar nossos espaços para conhecer novos investimentos e tendências."
+        title='Construa sua carteira de investimentos'
+        description='A Finscore traz cursos de como, onde e quando investir seu dinheiro para que você saia das dívidas, tenha uma reseva de emergencia e investa seu dinheiro para planos futuros.'
+        subDescription='Além disso você pode usar nossos espaços para conhecer novos investimentos e tendências.'
+        image={HomepageImage4}
       />
       <Contact />
       <Footer />
     </div>
-  );
+  )
 }
 
-export default Hero;
+export default Hero
